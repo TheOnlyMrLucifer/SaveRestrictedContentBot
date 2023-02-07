5b6b15c2fff53af17aaf0d3ab729546786b0289b
Chloe Decker <69217587+TheOnlyMrLucifer@users.noreply.github.com>
2023-02-07 23:22:00 +0100

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None, cast=int)
+API_ID = "3072251"
+API_HASH = "23cbf2ffb2404519b1172a7b5394ca13"
+BOT_TOKEN = "6187668461:AAF5Zk_geQe97MCO6WZ5KS_9nvnTujW88I8"
+SESSION = "BAA9ROpNBqUn0IoSmROUJXD8bwj4ZLQR7BfwcqFrWRWXSHouoLHIGOSUr5twjFfM2HZBWRqJ8g-21bpQU2vrPM0HWAdfHLGFyAmtRDFWpNu6_iYOcgBPTkFG-D8o9kKFxmrUfO5ptvLnznaGT7b-qoJqXjipCw7AYrxmowHusSqtXyfl_snUvBumV0ZIoMMXEoYhuNZcWFYHg9alt3_Yq7tI51be5VdBqRDKiyPLD_DUooGacEC_dWGCOuRGi7EccFCG-fMBH9qc9s66ea0HbICL_owrf_jZTk7BU8mHZuPNYbv3INzMd_GdhlTOfIJtZ-FwYLjj3MkVatQGPikheLQcS8RKoQA"
+FORCESUB = "ThePornLeague"
+AUTH = "1271155361"
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
